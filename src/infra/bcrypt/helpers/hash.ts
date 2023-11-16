f01004b559fad236ba3dd1f286b41db705757741
AyresMonteiro <main@ayresmonteiro.com>
2023-11-16 13:41:18 -0300

    feat: BcryptHashHelper implements CompareHashStringAction

--- src/infra/bcrypt/helpers/hash.ts
+++ src/infra/bcrypt/helpers/hash.ts
@@ -1,11 +1,20 @@
 import bcrypt from "bcrypt";
 
 import { HashStringAction } from "../../../data/string/hash";
+import { CompareHashStringAction } from "../../../data/string/compare-hash";
 
-export class BcryptHashHelper implements HashStringAction {
+export class BcryptHashHelper
+  implements HashStringAction, CompareHashStringAction
+{
   async hash(data: string): Promise<string> {
     const hash = await bcrypt.hash(data, 10);
 
     return hash;
+  }
+
+  async compare(string: string, hash: string): Promise<boolean> {
+    const isValid = await bcrypt.compare(string, hash);
+
+    return isValid;
   }
 }
