9cea2c6cff7c323a75cfcb1167aeee20f660a941
AyresMonteiro <main@ayresmonteiro.com>
2023-11-16 11:02:16 -0300

    fix: make props of SaveUserAction's input required

--- src/data/user/save.ts
+++ src/data/user/save.ts
@@ -3,5 +3,7 @@ import { User } from "../../domain/types/user";
 import { UserEntity } from "../../domain/entities/user";
 
 export interface SaveUserAction {
-  save(data: Pick<User, "email" | "name" | "password">): Promise<UserEntity>;
+  save(
+    data: Required<Pick<User, "email" | "name" | "password">>
+  ): Promise<UserEntity>;
 }
