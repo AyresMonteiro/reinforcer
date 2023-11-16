62a3e7b3048c0c9f61b32c31acad25cb481fc7d1
AyresMonteiro <main@ayresmonteiro.com>
2023-11-16 11:05:14 -0300

    test: CreateUser usecase should call hash action on password before save user

--- test/application/create-user.spec.ts
+++ test/application/create-user.spec.ts
@@ -9,49 +9,68 @@ import { faker } from "@faker-js/faker";
 
 import { CreateUser } from "../../src/application/usecases/create-user";
 
-import { InMemoryUserRepository } from "../../src/infra/in-memory/repositories/user";
+import { InMemoryUserRepository } from "../mock/infra/in-memory/repositories/user";
+import { MockHashStringHelper } from "../mock/infra/helper/string/hash";
 
 function makeSut() {
+  const hashStringAction = new MockHashStringHelper();
   const saveUserAction = new InMemoryUserRepository();
 
   const firstName = faker.person.firstName();
   const lastName = faker.person.lastName();
 
   return {
-    usecase: new CreateUser(saveUserAction),
+    usecase: new CreateUser(saveUserAction, hashStringAction),
     payload: {
       name: `${firstName} ${lastName}`,
       email: faker.internet.email({
         firstName,
         lastName,
       }),
+      password: faker.internet.password(),
+    },
+    dependencies: {
+      saveUserAction,
+      hashStringAction,
     },
   };
 }
 
-test("it should create user", async () => {
-  const { usecase, payload } = makeSut();
+describe("@application/usecases/create-user", () => {
+  test("it should create user", async () => {
+    const { usecase, payload } = makeSut();
 
-  const newUser = await usecase.execute(payload);
+    const newUser = await usecase.execute(payload);
 
-  expect(newUser).not.toStrictEqual(undefined);
-  expect(typeof newUser).toStrictEqual("object");
-  expect(newUser.name).toStrictEqual(payload.name);
-  expect(newUser.email).toStrictEqual(payload.email);
-  expect(newUser.password).toStrictEqual(undefined);
-});
+    expect(newUser).not.toStrictEqual(undefined);
+    expect(typeof newUser).toStrictEqual("object");
+    expect(newUser.name).toStrictEqual(payload.name);
+    expect(newUser.email).toStrictEqual(payload.email);
+    expect(newUser.password).toStrictEqual(undefined);
+  });
 
-test("it should create user with id", async () => {
-  const { usecase, payload } = makeSut();
+  test("it should create user with id", async () => {
+    const { usecase, payload } = makeSut();
 
-  const newUser = await usecase.execute(payload);
+    const newUser = await usecase.execute(payload);
 
-  expect(newUser).not.toStrictEqual(undefined);
-  expect(typeof newUser).toStrictEqual("object");
-  expect(newUser.id).not.toStrictEqual(undefined);
-  expect(newUser.id).not.toStrictEqual("");
-  expect(typeof newUser.id).toStrictEqual("string");
-  expect(newUser.name).toStrictEqual(payload.name);
-  expect(newUser.email).toStrictEqual(payload.email);
-  expect(newUser.password).toStrictEqual(undefined);
+    expect(newUser).not.toStrictEqual(undefined);
+    expect(typeof newUser).toStrictEqual("object");
+    expect(newUser.id).not.toStrictEqual(undefined);
+    expect(newUser.id).not.toStrictEqual("");
+    expect(typeof newUser.id).toStrictEqual("string");
+    expect(newUser.name).toStrictEqual(payload.name);
+    expect(newUser.email).toStrictEqual(payload.email);
+    expect(newUser.password).toStrictEqual(undefined);
+  });
+
+  test("it should call hash on password one time", async () => {
+    const { usecase, payload, dependencies } = makeSut();
+
+    const hashSpy = jest.spyOn(dependencies.hashStringAction, "hash");
+
+    await usecase.execute(payload);
+
+    expect(hashSpy).toHaveBeenCalledTimes(1);
+  });
 });
