c5862e18c58e90bd91ce4416d46cf3b266df2ace
AyresMonteiro <main@ayresmonteiro.com>
2023-11-16 13:54:29 -0300

    mock: MockAuthHelper implements ValidateCredentialsAction

--- test/mock/infra/helper/auth/credentials.ts
+++ test/mock/infra/helper/auth/credentials.ts
@@ -1,12 +1,35 @@
 import { GetCredentialsAction } from "../../../../data/auth/get-credentials";
+import { ValidateCredentialsAction } from "../../../../data/auth/validate-credentials";
 import { Authorization } from "../../../../data/types/auth/authorization";
 import { User } from "../../../../domain/types/user";
 
-export class MockAuthHelper implements GetCredentialsAction {
+export class MockAuthHelper
+  implements GetCredentialsAction, ValidateCredentialsAction
+{
+  private sessions: Record<string, Authorization>;
+
+  constructor() {
+    this.sessions = {};
+  }
+
   async getCredentials(data: User): Promise<Authorization> {
-    return {
+    const session = Math.random().toString(36);
+
+    this.sessions[session] = {
       user: data,
-      session: "any_session",
+      session,
     };
+
+    return this.sessions[session];
+  }
+
+  async validate(session: string): Promise<Authorization | null> {
+    const authorization = this.sessions[session];
+
+    if (!authorization) {
+      return null;
+    }
+
+    return authorization;
   }
 }
