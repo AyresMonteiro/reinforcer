4b6eabb70201862c5780c3be833ab601513d99ec
AyresMonteiro <main@ayresmonteiro.com>
2023-11-16 11:50:24 -0300

    style: respecting types dir convention

--- src/application/usecases/create-user.ts
+++ src/application/usecases/create-user.ts
@@ -2,7 +2,7 @@ import { SaveUserAction } from "../../data/user/save";
 
 import { Usecase } from "../contracts/usecase";
 import { HashStringAction } from "../../data/string/hash";
-import { CreateUserData } from "../@types/create-user";
+import { CreateUserData } from "../types/create-user";
 import { User } from "../../domain/types/user";
 
 export class CreateUser implements Usecase<CreateUserData, User> {
