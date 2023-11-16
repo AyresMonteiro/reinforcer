09cf3b9c1044e303fc539adf49e5be0e4c26b70c
AyresMonteiro <main@ayresmonteiro.com>
2023-11-16 11:02:49 -0300

    feat: add hash mechanism to create-user usecase

--- src/application/usecases/create-user.ts
+++ src/application/usecases/create-user.ts
@@ -1,18 +1,21 @@
-import { User } from "../../domain/types/user";
-import { UserEntity } from "../../domain/entities/user";
-
 import { SaveUserAction } from "../../data/user/save";
 
 import { Usecase } from "../contracts/usecase";
+import { HashStringAction } from "../../data/string/hash";
+import { CreateUserData } from "../@types/create-user";
+import { User } from "../../domain/types/user";
 
-export class CreateUser implements Usecase<User, UserEntity> {
-  constructor(private readonly saveUser: SaveUserAction) {}
+export class CreateUser implements Usecase<CreateUserData, User> {
+  constructor(
+    public readonly saveUser: SaveUserAction,
+    public readonly hashString: HashStringAction
+  ) {}
 
-  async execute(
-    data: Pick<User, "name" | "email" | "password">
-  ): Promise<UserEntity> {
+  async execute(data: CreateUserData): Promise<User> {
+    data.password = await this.hashString.hash(data.password);
+
     const newUser = await this.saveUser.save(data);
 
-    return newUser;
+    return newUser.plain();
   }
 }
